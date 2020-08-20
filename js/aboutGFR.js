067797a3769cc3c96ae09a050b93bd1e0300da1a
signalorion <masa4@daum.net>
2020-08-20 14:23:28 +0900

    제이쿼리업데이트

---
+++ js/aboutGFR.js
@@ -0,0 +1,188 @@
+//aboutGFR 제이쿼리 구간
+
+//전역변수////////////////
+//1. 페이지번호를 먹이는 전역변수
+var pno = 0;
+//2. 전체 페이지 수
+const totnum = 3;
+//const는 변수 var와 달리 변경불가한 상수를 말한다
+//3. 광스크롤방지
+var psts = 0; //0은 허용이고 1은 불허용이다
+//4. 한페이지 높이값
+var winH = $(window).height();
+//5. 마지막 페이지 스크롤 막기상태값
+var lastSts = 0; //0-허용, 1-막기
+
+
+//////////////////////////////////////
+
+$(function () {
+
+
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+
+        if (lastSts === 1) return true;
+
+        /////// 광스크롤막기 /////////////////
+        if (psts === 1) return true; //돌아가!
+        psts = 1; //잠금(기존0값을 변경)
+        setTimeout(function () {
+            psts = 0;
+        }, 600); /// 타임아웃 ///
+        ////////////////////////////////////
+
+
+
+        e = window.event || e;
+        var delta = e.detail ? e.detail : e.wheelDelta;
+        //console.log("델타:"+delta);
+        //아랫방향
+        if (delta < 0) {
+            pno++;
+            if (pno === totnum) pno = totnum - 1;
+        } else {
+            pno--;
+            if (pno === -1) pno = 0;
+        }
+        console.log("pno:" + pno);
+
+        $("html,body").stop().animate({
+            scrollTop: (winH * pno) + "px"
+        }, 400);
+
+        //마지막 페이지에서 자동스크롤 잠금
+        if (pno === 2) lastSts = 1;
+    }); ///////// mousewheel ///////////////////
+    ///////////////////////////////////////////
+
+    $(".discograp").scroll(function () {
+        var scTop = $(this).scrollTop();
+        console.log("스크롤위치:" + scTop);
+
+        // 마지막 페이지의 스크롤이 상단0에 도착하면 자동스크롤 잠금 상태 풀기
+        if (scTop === 0) lastSts = 0;
+
+    }); /////// scroll /////////////////
+
+
+
+
+
+    $(".detail_close").click(function () {
+        $(".albumdetail").hide();
+    }); //앨범상세페이지 누르면 닫기//////
+
+    $(".discograp ul li").mouseover(function () {
+        $(this).find("div").show();
+    }).mouseout(function () {
+        $(".albumimg_hover").hide();
+    });
+
+
+
+    $(".proclose").click(function () {
+        $(".member_info").hide(); //시간이징함수
+    }); ////멤버프로필 상세 누르면 닫기//////
+    
+    $(".member_info").click(function () {
+        $(".member_info").hide();
+    }); //그냥 멤버프로필을 눌러도 닫기//////
+
+    $(".sw_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.sw_pro").show(); //토글기능을쓰면서 팝업이랑 안겹치게 보이는 방법은 없을까...
+    });
+
+    $(".yr_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.yr_pro").show();
+    });
+    $(".eh_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.eh_pro").show();
+    });
+    $(".yj_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.yj_pro").show();
+    });
+    $(".sb_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.sb_pro").show();
+    });
+    $(".uj_pro>img").click(function () {
+        $(".member_info").hide(),
+            $(".member_info.uj_pro").show();
+    }); //각 멤버 사진 누르면 열ㄹㅣ기///
+
+
+    //////앨범디테일 켜기/////////////////////////
+    $("#albumimg_13").click(function () {
+        $(".albumdetail.no13").fadeIn(600);
+    });
+    $("#albumimg_12").click(function () {
+        $(".albumdetail.no12").fadeIn(600);
+    });
+    $("#albumimg_11").click(function () {
+        $(".albumdetail.no11").fadeIn(600);
+    });
+    $("#albumimg_10").click(function () {
+        $(".albumdetail.no10").fadeIn(600);
+    });
+    $("#albumimg_9").click(function () {
+        $(".albumdetail.no9").fadeIn(600);
+    });
+    $("#albumimg_8").click(function () {
+        $(".albumdetail.no8").fadeIn(600);
+    });
+    $("#albumimg_7").click(function () {
+        $(".albumdetail.no7").fadeIn(600);
+    });
+    $("#albumimg_6").click(function () {
+        $(".albumdetail.no6").fadeIn(600);
+    }); //////////////////앨범티테일켜기/////////
+
+    //앨범디테일창 클릭하면 꺼지게
+    $(".albumdetail").click(function () {
+        $(".albumdetail").fadeOut(500);
+    }); ///////////////////////////////앨범디테일끄기
+
+
+    //스크롤 부드럽게 이동하기~!
+    $(".cont1_menu a").click(function (e) {
+        //1. a링크 눌렀을때 기능을 제거한다.
+        e.preventDefault();
+
+        //2. a 태그의 href값을 읽어오기(페이지id값과도 같다)
+        var pid = $(this).attr("href");
+        //console.log(pid);
+
+        //3. 이동할 페이지 id 높이값을 구하기 offset() 메서드사용!
+        //높이값구하는 변수를 하나 만든다
+        var ptop = $(pid).offset().top; //top에 ()를 안쓰도록 주의하자 ^^
+        console.log(ptop);
+
+        //4. 스크롤이동 애니메이션 만들기
+        $("html, body").animate({
+            scrollTop: ptop + "px" //여기에 세미콜론을 안쓰도록 주의하자 ^^
+        }, 800);
+
+    }); /////////click/////////////////////
+
+    /////메인으로 돌아가는 화살표 애니메이션
+    $(".back2main").mouseenter(function () {
+        $(".back2main img").css({
+            transform: 'rotate(-90deg)',
+            transition: "all .8s"
+        });
+    });
+    $(".back2main").mouseleave(function () {
+        $(".back2main img").css('transform', 'rotate(0deg)');
+    }); ////////////////////화살표애니메이션
+
+
+
+
+
+
+
+}); //////제이쿼리로딩구간//////////////
---
+++ js/gfriend.js
@@ -0,0 +1,234 @@
+//메인화면 제이쿼리창
+
+//전역변수////////////////
+//1. 페이지번호를 먹이는 전역변수
+var pno = 0;
+//2. 전체 페이지 수
+var totnum = 6;
+//const는 변수 var와 달리 변경불가한 상수를 말한다
+//3. 광스크롤방지
+var psts = 0; //0은 허용이고 1은 불허용이다
+//4. 한페이지 높이값
+var winH = $(window).height();
+
+
+
+//////////////////////////////////////
+
+$(function () {
+    //스크롤로 화면 넘어가는거
+    //맨 밑에서 top가기 버튼 누르고나서 스크롤하면 다시 아래로 내려감
+    
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+
+
+        /////// 광스크롤막기 /////////////////
+        if (psts === 1) return true; //돌아가!
+        psts = 1; //잠금(기존0값을 변경)
+        setTimeout(function () {
+            psts = 0;
+        }, 600); /// 타임아웃 ///
+        ////////////////////////////////////
+
+
+        e = window.event || e;
+        var delta = e.detail ? e.detail : e.wheelDelta;
+        //console.log("델타:"+delta);
+        //아랫방향
+        if (delta < 0) {
+            pno++;
+            if (pno === totnum) pno = totnum - 1;
+        } else {
+            pno--;
+            if (pno === -1) pno = 0;
+        }
+        console.log("pno:" + pno);
+
+        $("html,body").stop().animate({
+            scrollTop: (winH * pno) + "px"
+        }, 400);
+
+
+
+    }); /////// mousewheel /////////////////
+    
+    
+
+    $(".mainlistlogo").hover(function () {
+        $("img", this).fadeOut(500)
+    }, function () {
+        $("img", this).fadeIn(900)
+    });
+    ///////////메인로고 사르르사라지기///////////
+
+
+    var tg1 = $(".slide1")
+    var goslide1 = function (dir) {
+        if (dir) {
+            var first = tg1.find("li").first();
+            tg1.append(first);
+        } else {
+            var last = tg1.find("li").last();
+            tg1.prepend(last);
+        }
+    }
+    var tg2 = $(".slide2")
+    var goslide2 = function (dir) {
+        if (dir) {
+            var first = tg2.find("li").first();
+            tg2.append(first);
+        } else {
+            var last = tg2.find("li").last();
+            tg2.prepend(last);
+        }
+    }
+    var tg3 = $(".slide3")
+    var goslide3 = function (dir) {
+        if (dir) {
+            var first = tg3.find("li").first();
+            tg3.append(first);
+        } else {
+            var last = tg3.find("li").last();
+            tg3.prepend(last);
+        }
+    }
+
+
+    $(".brkroom .img_arrow.right").click(function () {
+        goslide1(1);
+        
+
+    });
+
+    $(".tilted .img_arrow.right").click(function () {
+        goslide2(1);
+
+    });
+
+    $(".apple .img_arrow.right").click(function () {
+        goslide3(1);
+    });
+
+    $(".brkroom .img_arrow.left").click(function () {
+        goslide1(0);
+    });
+    $(".tilted .img_arrow.left").click(function () {
+        goslide2(0);
+    });
+    $(".apple .img_arrow.left").click(function () {
+        goslide3(0);
+    }); /////////슬라이드설정/////////////////
+
+    $(".track_plus").click(function () {
+        //화살표 돌아가는거 하고싶다
+        $(this).next().slideToggle(function () {});
+
+        /*$("img",this).css({transform: 'rotate(-45deg)', transition:"all .6s"});*/
+
+        $(".tracklist_tit.no1").css({
+            fontWeight: "bold"
+        });
+    });
+    $(".track_plus.no2").click(function () {
+
+        $(".tracklist_tit.no2").css({
+            fontWeight: "bold"
+        });
+    });
+    $(".track_plus.no3").click(function () {
+
+        $(".tracklist_tit.no3").css({
+            fontWeight: "bold"
+        });
+    });
+    $(".track_plus.no4").click(function () {
+
+        $(".tracklist_tit.no4").css({
+            fontWeight: "bold"
+        });
+    });
+    $(".track_plus.no5").click(function () {
+
+        $(".tracklist_tit.no5").css({
+            fontWeight: "bold"
+        });
+    });
+    $(".track_plus.no6").click(function () {
+
+        $(".tracklist_tit.no6").css({
+            fontWeight: "bold"
+        });
+    }); ////////신곡소개///////////////
+
+    $(".menubtn").mouseenter(function () {
+        /*width 55퍼로 늘리기*/
+        $(".menu_hover").css({
+            width: "40%",
+            transition: "all, .8s"
+        });
+    });
+    $(".menubtn").mouseout(function () {
+        /*width 55퍼로 늘리기*/
+        $(".menu_hover").css({
+            width: "0%",
+            transition: "all, .8s"
+        });
+    }); ////////메뉴버튼 설정///////////가만히좀있게못하나....
+
+    //페이지 스르륵 내려가기//////////////////////////////////
+    $(".nextpage a, #cont1 a, .mainlistlogo a").click(function (e) {
+        e.preventDefault();
+
+        var pid = $(this).attr("href");
+        console.log(pid);
+
+        var ptop = $(pid).offset().top;
+
+        $("html, body").animate({
+            scrollTop: ptop + "px"
+        }, 800);
+    }); ////////////로딩화면 화살표 클릭시///////////////
+
+    $(".concept_ver li a, .up_arrow a").click(function (e) {
+        e.preventDefault();
+
+        var pid = $(this).attr("href");
+        console.log(pid);
+
+        var ptop = $(pid).offset().top;
+
+        $("html, body").animate({
+            scrollTop: ptop + "px"
+        }, 800);
+    }); //////////////콘셉이름 클릭시////////////
+
+    $(".mainlinks .mainviewinfo").click(function (e) {
+        e.preventDefault();
+
+        var pid = $(this).attr("href");
+        console.log(pid);
+
+        var ptop = $(pid).offset().top;
+
+        $("html, body").animate({
+            scrollTop: ptop + "px"
+        }, 900);
+    }); //////////////메인메뉴 클릭시////////////
+
+    $(".up_arrow a").hover(function () {
+        $(this).animate({
+            bottom: "58px"
+        }, 500);
+    }, function () {
+        $(this).animate({
+            bottom: "50px"
+        });
+    }); ////////////////////메인으로가는 화살표 움직이기
+
+
+
+
+
+
+
+}); //////////제이쿼리로딩//////////////////
